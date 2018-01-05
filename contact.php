27975525a19db661d71bb7e614188e1d7a92d66e
Matthieu Duret <matthieu.duret@carboatmedia.fr>
2018-01-05 17:30:43 +0100

    v0.3 - fix map

--- contact.php
+++ contact.php
@@ -149,6 +149,9 @@
     </section> <!-- END section-->
 
 
+
+
+
     <section class="contact-bottom-content">
         <div class="container">
             <div class="row">
@@ -167,7 +170,50 @@
 
 
 
-<?php include('includes/footer.php'); ?>
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1 class="footer-theme-name">Matthieu</h1>
+                <p>
+                    © Copyright 2018
+                    <br>
+                    All rights reserved
+                </p>
+            </div>
+        </div> <!-- END row-->
+    </div> <!-- END container-->
+</footer> <!-- END site-footer-->
+
+
+
+
+
+<div class="scroll-top">
+    <span class="lnr lnr-chevron-up"></span>
+</div>
+
+
+
+
+
+
+<!--scripts-->
+<script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="assets/js/tether.min.js"></script>
+<script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/vendor/bootstrap-validator/dist/validator.min.js"></script>
+<script src="assets/vendor/typist/typist.js"></script>
+<script src="assets/vendor/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="assets/vendor/isotope/dist/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+<script src="assets/vendor/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="assets/vendor/wow/dist/wow.min.js"></script>
+<script src="assets/js/imagesloaded.js"></script>
+<script src="assets/js/smothscroll.js"></script>
+<script src="assets/js/count.js"></script>
+<script src="assets/js/theme.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?&amp;key=AIzaSyB0uuKeEkPfAo7EUINYPQs3bzXn7AabgJI"></script>
 
 <script>
 
